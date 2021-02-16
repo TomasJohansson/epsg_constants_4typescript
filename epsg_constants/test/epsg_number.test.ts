a31bca070981b6df6abb26c6860c6545ca407c84
Tomas Johansson <github-tomas@programmerare.com>
2021-02-16 11:59:15 +0100

    Added a class 'EpsgNumber' with some initial constants, and a test file, which verified that the project has now been properly configured, and can now execute tests for TypeScript code with the following command: 'pnpm run test'

---
+++ epsg_constants/test/epsg_number.test.ts
@@ -0,0 +1,21 @@
+﻿import EpsgNumber from '../src/epsg_number';
+
+test('some EPSG constants', () => {
+  expect(
+    EpsgNumber.WORLD__WGS_84__4326
+  ).toEqual(
+    4326
+  );
+
+  expect(
+    EpsgNumber.SWEDEN__SWEREF99_TM__3006
+  ).toEqual(
+    3006
+  );
+
+  expect(
+    EpsgNumber.SWEDEN__12_00__SWEREF99_12_00__3007
+  ).toEqual(
+    3007
+  );
+});
