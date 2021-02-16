31d9bfbc709d885474ce618445eeef2a5c4c1aaa
Tomas Johansson <github-tomas@programmerare.com>
2021-02-16 12:18:34 +0100

    Updated 'tsconfig.json' to include 'd.ts' files, and to generate output to the directory 'srcjs'. Also generated output to that directory with the command 'pnpx tsc'. Also configured 'package.json' with 'main' and 'types' file pointing to generated files in that 'srcjs' directory.

---
+++ epsg_constants/srcjs/src/epsg_number.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EpsgNumber = /** @class */ (function () {
+    function EpsgNumber() {
+    }
+    EpsgNumber.WORLD__WGS_84__4326 = 4326;
+    EpsgNumber.SWEDEN__SWEREF99_TM__3006 = 3006;
+    EpsgNumber.SWEDEN__12_00__SWEREF99_12_00__3007 = 3007;
+    return EpsgNumber;
+}());
+exports.default = EpsgNumber;
