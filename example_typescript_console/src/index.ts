0249980d4ad0382a191fc8bf5ae4393b806f35da
Tomas Johansson <github-tomas@programmerare.com>
2021-02-16 12:28:36 +0100

    Generated a new example module "example_typescript_console" with these four commands: pnpm init -y pnpm install typescript ts-node -D pnpx tsc --init pnpm install ../epsg_constants
    The above init commands created the files 'package.json' and 'tsconfig.json', and the install commands added some modules to 'package.json'.
    Also added a new file 'src/index.ts' which uses the module 'epsg_constants' (referred to by the above 'pnpm install ../epsg_constants')
    and added the following line in 'package.json' within 'scripts':
    "start": "ts-node ./src/index.ts",
    Now the above example file, using the module 'epsg_constants', can be run with the command 'pnpm run start'

---
+++ example_typescript_console/src/index.ts
@@ -0,0 +1,3 @@
+import EpsgNumber from 'epsg_constants';
+console.log("EpsgNumber WGS84 : " + EpsgNumber.WORLD__WGS_84__4326);
+console.log("EpsgNumber SWEREF99TM : " + EpsgNumber.SWEDEN__SWEREF99_TM__3006);
